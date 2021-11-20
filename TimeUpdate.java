050b4f2ceec4a8936f367416933cb2501658166e
asg0826 <94029405+asg0826@users.noreply.github.com>
2021-11-20 00:28:31 -0500

    Create TimeUpdate.java

---
+++ TimeUpdate.java
@@ -0,0 +1,45 @@
+import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class TimeUpdate {
+	
+	//Instances
+	Timer timer;
+	
+	//Constructor
+	public TimeUpdate(Long start, Long seconds) {
+		Timer timer = new Timer(); 
+		this.timer=timer;
+		timer.schedule(new timerTask(), 0, seconds);
+    	
+	 	
+	}
+	
+	public void Stop() {
+		
+		this.timer.cancel();
+	}
+	
+	
+    class timerTask extends TimerTask {
+        public void run() {
+        	
+        	Darrow darrow = new Darrow();
+        	
+			try {
+				
+				darrow.update();
+				
+			} catch (IOException e) {
+			
+				e.printStackTrace();
+			}
+        }
+    }
+	
+	//Methods
+	
+	
+
+}
