ae69ee001ead0518ca4227d1339f4592550d9775
asg0826 <94029405+asg0826@users.noreply.github.com>
2021-12-07 21:59:17 -0500

    Create Main.java

---
+++ Eo/Main.java
@@ -0,0 +1,32 @@
+import java.io.IOException;
+
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+    	
+    	
+
+        try {
+            TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+            botsApi.registerBot(new Eo());
+            
+            Eo Eo= new Eo();
+            
+          //  Eo.sendMsg();
+            
+            
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }  
+        
+        
+    }
+        
+	}
